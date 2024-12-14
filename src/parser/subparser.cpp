5efa60ef21d0ebb97deeaf364729491978fb9f87
xiaotianxt <tianyp@pku.edu.cn>
2024-12-14 14:42:19 +0800

    fix: remove flow to support Stash

--- src/parser/subparser.cpp
+++ src/parser/subparser.cpp
@@ -198,7 +198,7 @@ void vlessConstruct(Proxy &node, const std::string &group, const std::string &re
     node.EncryptMethod = cipher;
     node.TransferProtocol = net.empty() ? "tcp" : type == "http" ? "http" : net;
     node.Edge = edge;
-    node.Flow = flow;
+    // node.Flow = flow;
     node.FakeType = type;
     node.TLSSecure = tls == "tls" || tls == "xtls" || tls == "reality";
     node.PublicKey = pbk;
